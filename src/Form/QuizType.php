0f7fdfc75778a179577623456f93da8d1304adb0
Théo Dabin <theo.dabin78@gmail.com>
2022-05-18 15:23:00 +0200

    Creation de quiz OK

--- src/Form/QuizType.php
+++ src/Form/QuizType.php
@@ -8,20 +8,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class QuizType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('image')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du quiz'
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description'
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false
+            ])
             ->add('questions', CollectionType::class, [
                 'entry_type' => QuestionType::class,
                 'allow_add' => true,
                 'prototype' => true,
-                'mapped' => false
+                'mapped' => false,
+                'label' => false
                 ])
         ;
     }
